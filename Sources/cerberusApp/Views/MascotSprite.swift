19c0a19a219d95c143ab3810a0a4352735388ed4
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-12 00:08:35 +0800

    Enlarge menu bar mascot sprite

--- Sources/cerberusApp/Views/MascotSprite.swift
+++ Sources/cerberusApp/Views/MascotSprite.swift
@@ -3,7 +3,7 @@ import SwiftUI
 import cerberusCore
 
 enum MascotSprite: String, CaseIterable {
-    static let menuBarImageSize = NSSize(width: 14, height: 14)
+    static let menuBarImageSize = NSSize(width: 21, height: 21)
 
     case r1c1
     case r1c2
